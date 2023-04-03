24bed085cad5ef21313e6fa571a8ba4c8f9652f6
TechChip <42796435+techchipnet@users.noreply.github.com>
2023-04-03 21:08:21 +0530

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,6 +1,5 @@
 <?php
   $data = json_decode(file_get_contents('php://input'), true);
-  // Do something with the data here
   header('Content-type: text/html');
   file_put_contents('data.txt', $data, FILE_APPEND);
 ?>
